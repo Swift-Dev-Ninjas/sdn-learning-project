bc2d74e1305fa9e4d29fcf37e11c3a1d50610bd4
Christian Slanzi <christian.slanzi@gmail.com>
2021-05-09 16:49:44 +0200

    add resources

--- TestProjects/UI/RecipeBook/RecipeBook/Model/Recipe.swift
+++ TestProjects/UI/RecipeBook/RecipeBook/Model/Recipe.swift
@@ -8,17 +8,17 @@
 public struct Recipe: Equatable {
     
     public var id: Int
+    public var title: String
+    public var image: String
+    public var imageType: String
     public var calories: Int?
     public var carbs: String?
     public var fat: String?
-    public var image: String
-    public var imageType: String
     public var protein: String?
-    public var title: String
     
-    public init(id: Int, calories: Int? = nil, carbs: String? = nil,
-                fat: String? = nil, image: String, imageType: String,
-                protein: String? = nil, title: String) {
+    public init(id: Int, title: String, image: String, imageType: String, calories: Int? = nil, carbs: String? = nil,
+                fat: String? = nil,
+                protein: String? = nil) {
         self.id = id
         self.calories = calories
         self.carbs = carbs
@@ -42,23 +42,23 @@ public struct Recipe: Equatable {
         
         guard let dictionary = dictionary,
             let id = dictionary[idKey] as? Int,
+            let title = dictionary[titleKey]  as? String,
+            let image = dictionary[imageKey]  as? String,
+            let imageType = dictionary[imageTypeKey]  as? String,
             let calories = dictionary[caloriesKey] as? Int,
             let carbs = dictionary[carbsKey] as? String,
             let fat = dictionary[fatKey] as? String,
-            let image = dictionary[imageKey]  as? String,
-            let imageType = dictionary[imageTypeKey]  as? String,
-            let protein = dictionary[proteinKey]  as? String,
-            let title = dictionary[titleKey]  as? String else {
+            let protein = dictionary[proteinKey]  as? String else {
                 return nil
         }
                 
         self.id = id
+        self.title = title
+        self.image = image
+        self.imageType = imageType
         self.calories = calories
         self.carbs = carbs
         self.fat = fat
-        self.image = image
-        self.imageType = imageType
         self.protein = protein
-        self.title = title
     }
 }
